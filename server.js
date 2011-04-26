c44f50c33bbcd66e18a1a8663b3196bd4622ada5
Clint Andrew Hall <clint@clintandrewhall.com>
2011-04-25 21:28:50 -0500

    Wrote a test

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-var http = require('http');
+var sys = require("sys"),
+  http = require('http'),
+  time = require("time");
+
+function test() {
+  var d = new time.Date(), result = "";
+  result += "\n\n";
+  result += "Creating a date:\n";
+  result += "================\n\n";
+  result += "Date:     " + d.toString();
+  result += "\n\n";
+  result += "Changing the Timezone to 'America/Los_Angeles:\n";
+  result += "==============================================\n\n";
+  d.setTimezone("America/Los_Angeles");
+  result += "Date:     " + d.toString();
+  result += "\n";
+  result += "Timezone: " + d.getTimezoneAbbr();
+  return result;
+}
+
+sys.log(test());
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (nodester-nodetime-test) is running..');
+  res.end(test());
 }).listen(9625);
